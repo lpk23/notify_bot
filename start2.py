27367842b7fe3f4add28aa4e0ce273c1d7cfc50e
EVG <olpol.ozoro@gmail.com>
2022-10-31 11:10:28 +0300

    ПРосто

--- start2.py
+++ start2.py
@@ -1,24 +1,31 @@
 import json
+import random
 import time
 
 import telebot
 
 
 def load_settings():
-    with open("settings.json", 'r', encoding='UTF8') as f:
+    with open("settings.json", 'r', encoding='UTF-8') as f:
         data = json.load(f)
     return data
 def send_message():
-    with open(settings['file'],'r',encoding='UTF8') as f:
+    with open(settings['file'],'r',encoding='UTF-8') as f:
         data=f.read()
     return data
 
 if __name__ == '__main__':
     settings = load_settings()  # Получение данных из конфига
     bot = telebot.TeleBot(settings['api_key'])
-    for i in range(0,5):
+    message=send_message()
+    for i in range(0,1):
         for a in settings['chat_id']:
-            m=bot.send_message(a,f"Сообщение №{i}")
-            bot.pin_chat_message(a,m.message_id)
-        time.sleep(10)
+            if message!=settings['last_name']:
+                m=bot.send_message(a,f"{message}")
+                bot.pin_chat_message(a,m.message_id)
+                settings['last_name']=message
+            else:
+                print("Плохо")
+    with open('settings.json','w', encoding='UTF-8') as f:
+        json.dump(settings,f,indent=4)
 
